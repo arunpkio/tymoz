f085bd5ff1f1d1c4426eec1666c8a168a76fb3e8
Arun PK <arunpkroses@gmail.com>
2024-06-06 20:25:16 +0200

    Save and load selected cities information locally in cache.json file

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,29 @@ slint::include_modules!();
 mod time_zones;
 use chrono::{Local, Offset, Utc};
 use chrono_tz::Tz;
-use slint::{Model, Timer, TimerMode, VecModel};
+use directories::ProjectDirs;
+use serde_json;
+use slint::{CloseRequestResponse, Model, Timer, TimerMode, VecModel};
 use std::cell::RefCell;
+use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::io::{BufWriter, Write};
+use std::path::PathBuf;
 use std::rc::Rc;
 use std::str::FromStr;
 use time_zones::TIME_ZONES;
+
+pub const CACHE_FILE_NAME: &str = "cache.json";
+fn get_app_dir() -> PathBuf {
+    if let Some(proj_dirs) = ProjectDirs::from("com", "apk", "tymoz") {
+        let app_dir = proj_dirs.config_dir();
+        fs::create_dir_all(app_dir).expect("Failed to create application directory");
+        app_dir.to_path_buf()
+    } else {
+        panic!("Could not determine application directory");
+    }
+}
 
 fn format_time_diff(value: f64) -> String {
     match value {
@@ -19,7 +37,6 @@ fn format_time_diff(value: f64) -> String {
 struct AppState {
     available_cities_model: Rc<VecModel<TimeZoneInfo>>,
     selected_cities_model: Rc<VecModel<TimeZoneInfo>>,
-
     timer: Timer,
 }
 
@@ -47,17 +64,31 @@ impl AppState {
         let local_offset: f64 = utc_now.offset().fix().local_minus_utc() as f64 / 3600.0;
         let utc_date = utc_now.format("%d, %B").to_string();
         let utc_zone_info = &TIME_ZONES[0]; // By default, show UTC time
-        self.selected_cities_model.insert(
-            0,
-            TimeZoneInfo {
-                city: utc_zone_info.name.into(),
-                timenow: local_time_str.into(),
-                offset: format_time_diff(local_offset).into(),
-                date: utc_date.into(),
-                is_ahead: local_offset >= 0.0,
-                timezone: utc_zone_info.location.into(),
-            },
-        );
+
+        let app_dir = get_app_dir();
+        let settings_file = app_dir.join(CACHE_FILE_NAME);
+        if settings_file.exists() {
+            let file = File::open(settings_file).expect("Unable to open file");
+            let reader = BufReader::new(file);
+
+            for line in reader.lines() {
+                let line = line.expect("Unable to read line");
+                let info: TimeZoneInfo = serde_json::from_str(&line).expect("Unable to parse JSON");
+                self.selected_cities_model.push(info);
+            }
+        } else {
+            self.selected_cities_model.insert(
+                0,
+                TimeZoneInfo {
+                    city: utc_zone_info.name.into(),
+                    timenow: local_time_str.into(),
+                    offset: format_time_diff(local_offset).into(),
+                    date: utc_date.into(),
+                    is_ahead: local_offset >= 0.0,
+                    timezone: utc_zone_info.location.into(),
+                },
+            );
+        }
     }
 }
 
@@ -105,7 +136,7 @@ fn main() -> Result<(), slint::PlatformError> {
     data_adapter.set_selected_cities_model(app_state.borrow().selected_cities_model.clone().into());
 
     let state_copy = app_state.clone();
-    let app_window_weak = app_window.as_weak(); //.clone_strong();
+    let app_window_weak = app_window.as_weak();
     state_copy.borrow().timer.start(
         TimerMode::Repeated,
         std::time::Duration::from_millis(250),
@@ -119,11 +150,7 @@ fn main() -> Result<(), slint::PlatformError> {
                     if let Ok(tz) = Tz::from_str(&info.timezone) {
                         let zone_time = utc_now.with_timezone(&tz);
                         info.timenow = zone_time
-                            .format(if use_12h_format {
-                                "%I:%M %p"
-                            } else {
-                                "%H:%M"
-                            })
+                            .format(if use_12h_format { "%I:%M %p" } else { "%H:%M" })
                             .to_string()
                             .into();
                         app_state
@@ -147,6 +174,22 @@ fn main() -> Result<(), slint::PlatformError> {
 
     // _ to keep the timer alive
     let _timer = update(&app_window);
+
+    let selected_cities_model_clone2 = state_copy.borrow().selected_cities_model.clone();
+
+    app_window.window().on_close_requested(move || {
+        let app_dir = get_app_dir();
+        let cache_file = app_dir.join(CACHE_FILE_NAME);
+        let file = File::create(&cache_file).unwrap();
+        let mut binding = BufWriter::new(&file);
+        let writer: &mut &File = binding.get_mut();
+        for info in selected_cities_model_clone2.iter() {
+            let serialized = serde_json::to_string(&info).unwrap();
+            writeln!(writer, "{}", serialized).unwrap();
+        }
+        writer.flush().unwrap();
+        CloseRequestResponse::HideWindow
+    });
 
     app_window.run()
 }
